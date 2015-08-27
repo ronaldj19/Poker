45bf8eeb7a8314a07503061b72a4e7ba7c0f92f4
ronaldj <ronaldj@ronaldj-PC>
2015-08-27 03:59:06 -0500

    Inicio del proyecto

---
+++ Poker/src/main/java/PokerDesfalco/Poker/App.java
@@ -0,0 +1,13 @@
+package PokerDesfalco.Poker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
